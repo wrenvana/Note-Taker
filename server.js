66739e5983428dae4ba169d62a10d4f17195742f
Wren Sanchez <demisa00@gmail.com>
2022-06-14 17:12:20 -0700

    Added DELETE request.

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const { fstat } = require("fs");
 const path = require("path");
-const noteData = require('./db/db.json');
-// const notes = require('./notes.html') ?
+const noteData = require("./db/db.json");
 const app = express();
 const PORT = 3001;
 const { v4: uuidv4 } = require("uuid");
 
 app.use(express.json());
-
-
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -20,24 +16,45 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-//app.get to get request the API notes/get from the db.json
+//request the API notes/get from the db.json
 app.get('/api/notes', (req, res) => {
     //fs to read file from db folder where user's data is saved
     fs.readfile('db/db.json', "utf-8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
 });
-})
+});
 
 app.post('/notes', (req, res) => {
-    //title text and id that will be inputted and saved by the user
+    //title text and id submitted by user
     var createNote = {
         title:req.body.title,
         text:req.body.text,
         id:uuidv4(),
     }
-    //fs to read file and add to HTML to display current note
-    fs.readfile('db/db.json', "utf-8", (err, data) => {                                   
+    //fs read file and add to HTML
+    fs.readfile('db/db.json', "utf-8", (err, data) => { 
+        if (err) throw err;  
+        var dataNotes = JSON.parse(data);
+        dataNotes.push(createNote);
+        fs.writeFile("db/db.json", JSON.stringify(dataNotes), (err) => {
+        err ? console.log(err) : console.log("Note created.")
+        });
+        res.sendFile(path.join(__dirname, "/public/notes.html"));                              
+});
+});
+
+app.delete("/api/notes/:id", (req, res) =>{
+    var deleteNote = req.params.id;
+    fs.readfile("db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        var database = JSON.parse(data);
+        var newNote = database.filter((note) => note.id !== deleteNote);
+        fs.writeFile("db/db.json", JSON.stringify(newNote), (err) => {
+            err ? console.log(err) : console.log("Note deleted.");
+        });
+        res.sendFile(path.join(__dirname, "/public/notes.html"));
+    })
 })
 
 app.listen(PORT, () =>
