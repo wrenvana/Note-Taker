6cf608d41263ead91067516096e9a260279cf581
Wren Sanchez <demisa00@gmail.com>
2022-06-14 17:22:48 -0700

    Edited path names.

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const noteData = require("./db/db.json");
 const app = express();
 const PORT = 3001;
 const { v4: uuidv4 } = require("uuid");
-
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 //request the API notes/get from the db.json
 app.get('/api/notes', (req, res) => {
     //fs to read file from db folder where user's data is saved
-    fs.readfile('db/db.json', "utf-8", (err, data) => {
+    fs.readfile('./db/db.json', "utf-8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
 });
@@ -37,10 +37,10 @@ app.post('/notes', (req, res) => {
         if (err) throw err;  
         var dataNotes = JSON.parse(data);
         dataNotes.push(createNote);
-        fs.writeFile("db/db.json", JSON.stringify(dataNotes), (err) => {
+        fs.writeFile("./db/db.json", JSON.stringify(dataNotes), (err) => {
         err ? console.log(err) : console.log("Note created.")
         });
-        res.sendFile(path.join(__dirname, "/public/notes.html"));                              
+        res.sendFile(path.join(__dirname, "./public/notes.html"));                              
 });
 });
 
@@ -50,10 +50,10 @@ app.delete("/api/notes/:id", (req, res) =>{
         if (err) throw err;
         var database = JSON.parse(data);
         var newNote = database.filter((note) => note.id !== deleteNote);
-        fs.writeFile("db/db.json", JSON.stringify(newNote), (err) => {
+        fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
             err ? console.log(err) : console.log("Note deleted.");
         });
-        res.sendFile(path.join(__dirname, "/public/notes.html"));
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
     })
 })
 
