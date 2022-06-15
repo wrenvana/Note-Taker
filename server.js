28491df87501b54f5e2c47e1f9211ee9f7b05518
Wren Sanchez <demisa00@gmail.com>
2022-06-14 17:33:54 -0700

    Debugging paths

--- server.js
+++ server.js
@@ -20,23 +20,25 @@ app.get('/notes', (req, res) =>
 //request the API notes/get from the db.json
 app.get('/api/notes', (req, res) => {
     //fs to read file from db folder where user's data is saved
-    fs.readfile('./db/db.json', "utf-8", (err, data) => {
+    fs.readfile('db/db.json', "utf-8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
 });
 });
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     //title text and id submitted by user
-    var createNote = {
-        title:req.body.title,
-        text:req.body.text,
-        id:uuidv4(),
-    }
+    // var createNote = {
+    //     title:req.body.title,
+    //     text:req.body.text,
+    //     id:uuidv4(),
+    // }
     //fs read file and add to HTML
     fs.readfile('db/db.json', "utf-8", (err, data) => { 
         if (err) throw err;  
-        var dataNotes = JSON.parse(data);
+        // var dataNotes = JSON.parse(data);
+        const createNote = req.body;
+        newNote.id = uuidv4();
         dataNotes.push(createNote);
         fs.writeFile("./db/db.json", JSON.stringify(dataNotes), (err) => {
         err ? console.log(err) : console.log("Note created.")
